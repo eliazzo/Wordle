b86c37ba429f3f0e859df7d3dfdba8be138960d3
eliazzo <bethazz@hotmail.co.uk>
2023-03-09 16:29:39 +0000

    incomplete

--- wordle.js
+++ wordle.js
@@ -2,7 +2,7 @@
 
 const container = document.getElementById('grid-container');
 
-for (let row = 1; row <= 5; row++) {
+for (let row = 1; row <= 6; row++) {
   for (let col = 1; col <= 5; col++) {
     const cell = document.createElement('div');
     cell.classList.add('grid-cell');
@@ -12,11 +12,6 @@ for (let row = 1; row <= 5; row++) {
   }
 }
 
-
-// Correct word 
-const pinky = ['p', 'i', 'n', 'k', 'y']
-
-
 // Get all grid cells in the first column
 const firstColumnCells = container.querySelectorAll('[data-col*="1"]');
 
@@ -24,25 +19,70 @@ firstColumnCells.forEach(cell => {
     cell.style.border = "2px solid red";
 })
 
+// GridFirstRow NodeList (5)
 const gridFirstRow = container.querySelectorAll('[data-row*="1');
 
 gridFirstRow.forEach(cell => { 
   cell.style.border = "2px solid blue";
 })
 
-// select all alphabet tiles
+// Letter tiles NodeList (29)
 const letterTiles = document.querySelectorAll(".btn");
+console.log(letterTiles);
 
+// for (i=0; i<letterTiles; i++){
+//   letterTiles[i].addEventListener("click", () => {
 
+//     console.log(letterTiles[i]);
 
-letterTiles.forEach(tile => {
-  tile.addEventListener("click", () =>{
-    gridFirstRow.forEach(cell => {
-      if (cell.children.length === 0) {
-      cell.appendChild(tile);
-      }
-      else
-      console.log('failed');
-    })
-  })
+//     for(let i=0; i<gridFirstRow.length; i++){
+//     if (gridFirstRow[i].childElementCount === 0){
+//       gridFirstRow[i].appendChild(letterTiles[i]);
+//       }
+//     }
+
+// //   })
+// }
+
+letterTiles.forEach(tile => { // iterate through keyboard tiles
+  tile.addEventListener("click", () => {
+    console.log(tile.innerHTML);
+    const selectLetter = tile.innerHTML;
+      gridFirstRow.forEach(function (cell, index) {
+        if (cell.innerHTML === '' && index === 0){
+          cell.innerHTML = selectLetter;
+        }
+        if (cell.innerHTML === '' && index !== 0 && index === 1){
+          cell.innerHTML = selectLetter;
+        }
+        
+      })
+
+  //   if (gridFirstRow[0].innerHTML === ''){
+  
+  //   gridFirstRow[0].innerHTML = selectLetter;
+  //   }
+  //   else gridFirstRow[1].innerHTML = selectLetter;
+  //   if (gridFirstRow[1].innerHTML !== ''){
+  //     gridFirstRow[2].innerHTML = selectLetter;
+  //   }
+    
+  // })
 })
+})
+
+  // tile.addEventListener("click", (tile) => { // add event listener to each letter tile
+  //   for(let i=0; i<gridFirstRow.length; i++){ // iterate through first row
+  //     if (gridFirstRow[i].childElementCount === 0){ // if first row cell is empty
+  //       console.log(tile);
+  //       // const selectTile = tile[i];
+  //       // const selectTileCopy = document.createElement("div");
+  //       // selectTileCopy.innerHTML = selectTile.innerHTML
+
+
+  //     gridFirstRow[i].appendChild(tile);
+  //     }
+  //   }
+  //   })
+  // })
+
